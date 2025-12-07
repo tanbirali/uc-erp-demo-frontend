9003a13cf3f8ef87b4e2e1e064d36d69d3b484b7
tanbirali <tanbirali2004@gmail.com>
2025-12-07 21:04:33 +0530

    updated the dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,21 @@
+import { useAuth } from "../context/AuthContext";
+
 const Dashboard = () => {
+  const { user, logout } = useAuth();
   return (
     <div>
-      <div className="flex flex-col justify-center items-center">
-        <h1>Welcome to the Dashboard</h1>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-3xl">Welcome to the Dashboard</h1>
+        <div className="flex flex-col gap-4 p-6">
+          <p className="text-xl">User: {user?.first_name}</p>
+          <p className="text-xl">Email: {user?.email}</p>
+        </div>
+        <button
+          onClick={logout}
+          className="bg-red-500 text-white p-2 rounded hover:bg-red-600 hover:cursor-pointer"
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
